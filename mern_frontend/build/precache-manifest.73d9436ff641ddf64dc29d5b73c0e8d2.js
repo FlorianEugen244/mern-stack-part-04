d71746c6f7867e5ad95733e4b8e072e7549f0f69
eugen_frumosu <eugen.frumosu@mpg.com.ro>
2021-01-12 11:44:01 +0200

    First commit

---
+++ mern_frontend/build/precache-manifest.73d9436ff641ddf64dc29d5b73c0e8d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f484fbdd09b38296238b",
+    "url": "/static/css/main.167e0b31.chunk.css"
+  },
+  {
+    "revision": "f484fbdd09b38296238b",
+    "url": "/static/js/main.f484fbdd.chunk.js"
+  },
+  {
+    "revision": "53ab8203d2ea620f5fae",
+    "url": "/static/css/1.dc8a43cc.chunk.css"
+  },
+  {
+    "revision": "53ab8203d2ea620f5fae",
+    "url": "/static/js/1.53ab8203.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "13a2887f19b24f1879bcf233e8372a5f",
+    "url": "/index.html"
+  }
+];
